41c2eafe19e80b4f5addc051baf0d6d11b6b0514
vvchn <vl.soldatov4@gmail.com>
2024-05-10 06:30:11 +0300

    Fix typo in URL and add link to documentation

--- app/src/main/java/com/vvchn/vktestcase/data/remote/utils/Constants.kt
+++ app/src/main/java/com/vvchn/vktestcase/data/remote/utils/Constants.kt
@@ -1,7 +1,10 @@
 package com.vvchn.vktestcase.data.remote.utils
 
 object Constants {
-    const val BASE_URL = "https://pokeapi.co/api/v2/"
+    const val BASE_URL = "https://pokeapi.com/api/v2/"
     const val POKEMON = "pokemon"
+
+    /** "By default, a list "page" will contain up to 20 resources"
+     * See [here](https://pokeapi.co/docs/v2#resource-listspagination-section) for detailes  */
     const val DEFAULT_PAGE_SIZE = 20
 }

056fd575fce5ff62a1b47ca5dccd2b16218b55ce
vvchn <vl.soldatov4@gmail.com>
2024-05-10 07:23:29 +0300

    Fix "typo" in API URL

--- app/src/main/java/com/vvchn/vktestcase/data/remote/utils/Constants.kt
+++ app/src/main/java/com/vvchn/vktestcase/data/remote/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.vvchn.vktestcase.data.remote.utils
 
 object Constants {
-    const val BASE_URL = "https://pokeapi.com/api/v2/"
+    const val BASE_URL = "https://pokeapi.co/api/v2/"
     const val POKEMON = "pokemon"
 
     /** "By default, a list "page" will contain up to 20 resources"
